b097d9aaecf3062949ea4985040acd3c484f4a2b
calebbeandev <bondbean01@gmail.com>
2024-04-10 00:12:25 -0400

    Added plotting and jupytr, plan to transition fully soon

--- LULESH/job-hw.sh
+++ LULESH/job-hw.sh
@@ -3,12 +3,12 @@
 #SBATCH --mail-user=caleb.bean@ufl.edu
 #SBATCH --account=eel6763
 #SBATCH --qos=eel6763
-#SBATCH --nodes=8
-#SBATCH --ntasks=8
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=500mb
 #SBATCH -t 00:05:00
 #SBATCH -o lulesh_out1
 #SBATCH -e lulesh_err1
-srun ./lulesh2.0 -s 20
+srun ./lulesh2.0 -s 8
--- scripts/slurm.py
+++ scripts/slurm.py
@@ -5,12 +5,15 @@
 
 import subprocess
 import os
+import matplotlib.pyplot as plt
+import numpy as np
 
-job_count = 2 # Modify as additional parameter entries are placed in 'sbatch_cmds'
 
 job_directory = '../LULESH/'
 job_file_name = 'job-hw.sh'
 job_path = job_directory+job_file_name
+
+plot_directory = '../plots/'
 
 email = 'caleb.bean@ufl.edu'
 # Note that currently outfiles do not get produced and the output is only available via stdout
@@ -18,21 +21,40 @@ outfile_prefix = 'lulesh_out'
 errfile_prefix = 'lulesh_err'
 slurmrun_cmd = 'srun ./lulesh2.0 -s '
 
+# SBATCH parameters
+# Serial case shown below
+lulesh_s_param = [5,8]
+nodes_param = [1]*len(lulesh_s_param)
+n_tasks_param = [1]*len(lulesh_s_param)
+n_taskspernode_param = [1]*len(lulesh_s_param)
+
+job_count = len(lulesh_s_param)
+
+
+
+# Set one of these true for build
+make_cmd_dict = {
+            'SERIAL'    : True,
+            'MPI'       : False,
+            'OMP'       : False,
+            'MPI+OMP'   : False
+            }
+
 sbatch_cmds_dict = {
     '#!/bin/bash'                           : [],
     '#SBATCH --mail-type=FAIL'              : [],
     '#SBATCH --mail-user='+email            : [],
     '#SBATCH --account=eel6763'             : [],
     '#SBATCH --qos=eel6763'                 : [],
-    '#SBATCH --nodes='                      : [8,8,8], # Fill this
-    '#SBATCH --ntasks='                     : [8,8,8], # Fill this
-    '#SBATCH --ntasks-per-node='            : [1,1,1], # Fill this
+    '#SBATCH --nodes='                      : nodes_param, # Fill this
+    '#SBATCH --ntasks='                     : n_tasks_param, # Fill this
+    '#SBATCH --ntasks-per-node='            : n_taskspernode_param, # Fill this
     '#SBATCH --cpus-per-task=1'             : [],
     '#SBATCH --mem-per-cpu=500mb'           : [],
     '#SBATCH -t 00:05:00'                   : [],
     '#SBATCH -o '+outfile_prefix            : list(range(job_count)),
     '#SBATCH -e '+errfile_prefix            : list(range(job_count)),
-    slurmrun_cmd                            : [10,20,21], # Fill this
+    slurmrun_cmd                            : lulesh_s_param, # Fill this
     }
 
 # Outputs we wish to track (MUST be in form Foo=X where X is floating point data to be captured)
@@ -40,6 +62,25 @@ outputs_dict =   {
                 "Time" : [],
                 "Cycles" : []
                 }
+
+def buildCode(make_cmd_dict, job_dir):
+    for make_cmd, valid in make_cmd_dict.items():
+         if(valid):
+
+            if(make_cmd == "SERIAL"):
+                make = subprocess.run(['make clean -f serial_make.mk'], shell=True, cwd=job_dir)
+                make = subprocess.run(['make -f serial_make.mk'], shell=True, cwd=job_dir)
+
+            elif(make_cmd == "MPI"):
+                # NOT CURRENTLY WORKING (check the make file)
+                make = subprocess.run(['make clean -f mpi_make.mk'], shell=True, cwd=job_dir)
+                make = subprocess.run(['make -f mpi_make.mk'], shell=True, cwd=job_dir)
+            # else if(make_cmd == "OMP"):
+# 
+            # else if(make_cmd == "MPI+OMP"):
+
+            else:
+                raise ValueError('Incorrect input names to make cmd')
 
 def genSlurmCmds(sbatch_cmds_dict : dict, job_count : int):
     # This function converts the vectorized dictionary into individual string arrays to be written to slurm files
@@ -84,16 +125,38 @@ def parseOutputText(text: str, out_dict: str):
                 temp = temp.replace('=','')
                 out_dict[key].append(float(temp))
     
-    print(out_dict)
+    #print(out_dict)
 
 
-### TEST SETUP ###
+
+buildCode(make_cmd_dict, job_directory)
 
 slurm_cmds_arr = genSlurmCmds(sbatch_cmds_dict, job_count)
 
 for slurm_cmds in slurm_cmds_arr:
+
     genSlurmFile(slurm_cmds, job_path)
     output_text = executeSlurmFile(job_directory, job_file_name)
     parseOutputText(output_text, outputs_dict)
 
 
+#print(lulesh_s_param)
+#print(outputs_dict)
+
+t = np.array(outputs_dict['Time'])
+iterations = np.array(outputs_dict['Cycles'])
+
+print("time/iteration: ", t/iterations)
+
+t_per_iter = t/iterations
+
+fig, ax = plt.subplots()
+ax.plot(lulesh_s_param, t_per_iter)
+
+ax.set(xlabel='Problem size (length of cube mesh side)', ylabel='Time per iteration',
+       title='LULESH Serial Analysis')
+ax.grid()
+
+fig.savefig(plot_directory+"test.png")
+
+plt.show()
