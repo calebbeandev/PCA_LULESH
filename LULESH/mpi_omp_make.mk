10d17030d9f5e26f27bdbec1eb00e3063e93c31b
calebbeandev <bondbean01@gmail.com>
2024-04-17 23:51:46 -0400

    Cleaned python and added make files for each config

--- LULESH/mpi_omp_make.mk
+++ LULESH/mpi_omp_make.mk
@@ -22,8 +22,8 @@ SOURCES2.0 = \
 OBJECTS2.0 = $(SOURCES2.0:.cc=.o)
 
 #Default build suggestions with OpenMP for g++
-#CXXFLAGS = -g -pg -O3 -fopenmp -I. -Wall
-#LDFLAGS = -g -pg -O3 -fopenmp
+CXXFLAGS = -g -pg -O3 -fopenmp -I. -Wall
+LDFLAGS = -g -pg -O3 -fopenmp
 
 #Below are reasonable default flags for a serial build
 #CXXFLAGS = -g -pg -O3 -I. -Wall
