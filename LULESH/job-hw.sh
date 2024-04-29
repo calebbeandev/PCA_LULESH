deade34f2e8408b1006b40ea51172fdb2fcff416
calebbeandev <bondbean01@gmail.com>
2024-04-29 17:08:51 -0400

    Updated python for different configurations

--- LULESH/job-hw.sh
+++ LULESH/job-hw.sh
@@ -3,12 +3,14 @@
 #SBATCH --mail-user=caleb.bean@ufl.edu
 #SBATCH --account=eel6763
 #SBATCH --qos=eel6763
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=1
+#SBATCH --partition=hpg2-compute
+#SBATCH --nodes=
+#SBATCH --ntasks=64
+#SBATCH --ntasks-per-node=64
+#SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=500mb
-#SBATCH -t 00:05:00
-#SBATCH -o lulesh_out1
-#SBATCH -e lulesh_err1
-srun ./lulesh2.0 -s 8
+#SBATCH -t 00:20:00
+#SBATCH -o lulesh_out6
+#SBATCH -e lulesh_err6
+export OMP_NUM_THREADS=16
+srun ./lulesh2.0 -i 500 -s 32
