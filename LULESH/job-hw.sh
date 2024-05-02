61185fed8396e6ec465ae0ba8dcfdbdc4f30d720
calebbeandev <bondbean01@gmail.com>
2024-05-02 18:33:55 -0400

    final commit, hopefully :)

--- LULESH/job-hw.sh
+++ LULESH/job-hw.sh
@@ -3,14 +3,13 @@
 #SBATCH --mail-user=caleb.bean@ufl.edu
 #SBATCH --account=eel6763
 #SBATCH --qos=eel6763
-#SBATCH --partition=hpg2-compute
-#SBATCH --nodes=
-#SBATCH --ntasks=64
-#SBATCH --ntasks-per-node=64
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=16
 #SBATCH --cpus-per-task=16
 #SBATCH --mem-per-cpu=500mb
 #SBATCH -t 00:20:00
-#SBATCH -o lulesh_out6
-#SBATCH -e lulesh_err6
+#SBATCH -o lulesh_out4
+#SBATCH -e lulesh_err4
 export OMP_NUM_THREADS=16
-srun ./lulesh2.0 -i 500 -s 32
+srun ./lulesh2.0 -i 50 -s 30
